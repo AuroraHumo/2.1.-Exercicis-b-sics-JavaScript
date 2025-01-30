64c3ec7cbdabf4dfcbe9e3f025a9868dc737df85
AuroraHumo <carles.casallachs@gmail.com>
2025-01-30 13:09:52 +0100

    NIVELL 1. Exercici 2.
    NIVELL 1. Exercici 2.

--- Exercici 1.1 Arrow functions.js
+++ Exercici 1.1 Arrow functions.js
@@ -1,6 +1,15 @@
+
 // NIVELL 1
     // Exercici 1.
     // Convertir a funció fletxa:
     // function add(a, b) {return a + b;}
 
 const add = (a, b) =>  ( a + b )
+
+    // NIVELL 1. Exercici 2.
+    // Funció de fletxa sense paràmetres: 
+    // Crea una funció de fletxa anomenada randomNumber que no necessiti paràmetres i que retorni un número aleatori entre 0 i 100.
+
+const randomNumber = () => ( Math.floor(Math.random()*101))
+
+console.log(randomNumber())
